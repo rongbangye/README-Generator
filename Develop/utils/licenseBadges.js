40b6826aec547acb6f2b196d0493415b7b149ede
rongbang <rongbang.ye@gmail.com>
2020-09-20 20:06:26 -0700

    added validation and licensebadges

--- Develop/utils/licenseBadges.js
+++ Develop/utils/licenseBadges.js
@@ -6,6 +6,10 @@ const licenseBadgeLinks = {
     "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
   "Apache 2.0":
     "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  Eclipse:
+    "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+  IBM:
+    "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
   "GNU v3":
     "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
   "BSD 3-Clause":
