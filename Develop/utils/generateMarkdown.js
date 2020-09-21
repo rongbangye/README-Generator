5d923581092aff43e72194f1b22289dbd462309a
rongbang <rongbang.ye@gmail.com>
2020-09-20 18:11:13 -0700

    added generatedmarkdown and licenseBadges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,40 @@
+const licenseBadgeLinks = require("./licenseBadges");
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+  // set url for license badge
+  data.licenseBadge = licenseBadgeLinks[data.license];
+
+  // return markdown content
   return `# ${data.title}
+${data.licenseBadge}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install dependencies, run the following:
+\`
+${data.installation}
+\`
+## Usage
+${data.usage}
+## License
+This repository is licensed under the ${data.license} license.
+## Contributing
+${data.contribute}
+## Tests
+To run tests, run the following:
+\`
+${data.tests}
+\`
+## Questions
+Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work in GitHub at [${data.username}](https://github.com/${data.username}) 
 `;
 }
 
