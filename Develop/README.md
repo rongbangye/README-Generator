5aba3813e09ce851d1c860e9f21d1495ade63f39
rongbang <rongbang.ye@gmail.com>
2020-09-20 18:09:06 -0700

    added questions

---
+++ Develop/README.md
@@ -0,0 +1,29 @@
+# s
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+s
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install dependencies, run the following:
+`
+s
+`
+## Usage
+
+## License
+This repository is licensed under the MIT license.
+## Contributing
+
+## Tests
+To run tests, run the following:
+`
+npm run test
+`
+## Questions
+Questions about this repository? Please contact me at [s](mailto:s). View more of my work in GitHub at [s](https://github.com/s) 
--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,100 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkDown = require("./utils/generateMarkdown");
 
-];
+// set the fs.writeFile function to use promises
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your project's title?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project.",
+      name: "description"
+    },
+    {
+      type: "list",
+      message: "What license should your project have?",
+      name: "license",
+      choices: [
+        "MIT",
+        "Unlicense",
+        "Apache 2.0",
+        "GNU v3",
+        "BSD 3-Clause",
+        "Mozilla Public License 2.0"
+      ]
+    },
+    {
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "installation",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      message: "What command should be run to run tests?",
+      name: "tests",
+      default: "npm run test"
+    },
+    {
+      type: "input",
+      message: "What does the user need to know about using the repository?",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about contributing to the repository?",
+      name: "contribute"
+    }
+  ]);
+};
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+const writeToFile = (fileName, data) => {
+  return writeFileAsync(fileName, data);
+};
 
 // function to initialize program
-function init() {
+const init = async () => {
+  try {
+    console.log(
+      "Welcome to the README generator.\nPlease answer the following questions:"
+    );
 
-}
+    // ask user for answers to questions
+    const answers = await promptUser();
+
+    // create markdown content from user answers
+
+    // write markdown content to README.md file
+    await writeToFile("./README.md", generateMarkDown(answers));
+
+    // notify user that file has been written
+    console.log("README.md created.");
+  } catch (err) {
+    console.error("Error creating README. File not created.");
+    console.log(err);
+  }
+};
 
 // function call to initialize program
 init();
